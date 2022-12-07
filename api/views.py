bff790b22bdf276af15a26ca87e0d27ac0a1ec57
Shaxzod <painloversgame@gmail.com>
2022-12-07 22:38:32 +0500

    Libraru uzb

---
+++ api/views.py
@@ -0,0 +1,18 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from .serializers import *
+from products.models import *
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+import os
+
+
+def validate_file_extension(value):
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = ['.pdf', '.doc', '.docx']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension.')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=300, null=True)
+    image = models.ImageField(upload_to="Kutubxona_image", null=True)
+
+    def __str__(self):
+        return self.title
